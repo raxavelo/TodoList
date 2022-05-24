f94774dfe318c44a0edcf9c242e267a865f9f953
raxavelo <raxavelo@gmail.com>
2022-05-24 19:27:47 +0200

    Added tableView and addTaskButton to TodoListView

--- TodoList/TodoListViewController.swift
+++ TodoList/TodoListViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class TodoListViewController: UIViewController {
-
+  
+  @IBOutlet weak var addTaskButton: UIButton!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    addButtonShadow()
   }
 
+  private func addButtonShadow() {
+    addTaskButton.layer.shadowRadius = 10.0
+    addTaskButton.layer.shadowOpacity = 0.3
+  }
 
 }
 
