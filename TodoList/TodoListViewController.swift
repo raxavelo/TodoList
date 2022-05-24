c1f9c156459d68d141515cf6161da3bd25baf1fb
raxavelo <raxavelo@gmail.com>
2022-05-24 15:13:06 +0200

    Added NavigationController and TodoListNavigationController.

--- TodoList/TodoListViewController.swift
+++ TodoList/TodoListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TodoListViewController.swift
 //  TodoList
 //
 //  Created by Grzegorz Zadkowski on 24/05/2022.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TodoListViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
